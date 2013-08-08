9a8fda33ad1b7fedb9d3a7c671836723118f4bba
Yue Yu <admin@iyuyue.com>
2013-08-08 15:49:53 +0800

    data_preprocessor alpha

---
+++ data_preprocessing/demo.java
@@ -0,0 +1,43 @@
+import Jama.Matrix;
+import java.io.IOException;
+import pca_transform.*;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+
+public class demo {
+
+  public static void main( String[ ] args ) throws IOException {
+    FeatureMap test = new FeatureMap();
+    DecimalFormat formatter = new DecimalFormat("0.000000");
+
+    String trainingDataPath = "./data/test1.csv";
+    Matrix originalData = DataReader.read(test.getFile(trainingDataPath));
+    Matrix trainingData = originalData.getMatrix(0, originalData.getRowDimension()-1, 0, originalData.getColumnDimension()-2);
+
+    // System.out.println("originalData: "+originalData.getRowDimension()+"*"+originalData.getColumnDimension());
+    // System.out.println("trainingData: "+trainingData.getRowDimension()+"*"+trainingData.getColumnDimension());
+    
+    PCA pca = new PCA(trainingData);
+    Matrix transformedData =
+      pca.transform(trainingData, PCA.TransformationType.WHITENING);
+
+    FileWriter outputFile = new FileWriter("PreProcessed", true);
+    PrintWriter printer = new PrintWriter(outputFile);
+
+    // System.out.println("Output Data:");
+    for(int r = 0; r < transformedData.getRowDimension(); r++){
+      printer.print(originalData.get(r, originalData.getColumnDimension()-1)+" ");
+      for(int c = 0; c < transformedData.getColumnDimension(); c++){
+        printer.print(c+1+":"+formatter.format(transformedData.get(r,c))+" ");
+
+      }
+      printer.println("");
+    }
+
+    printer.close();
+    outputFile.close();
+    
+  } // main
+  
+} // class
